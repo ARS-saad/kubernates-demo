b9cd4f634eab7f948c7bc7c78220403d4d5900fc
Abdur Rahman Saad <abdurrahmansaad46@gmail.com>
2025-10-10 14:53:56 +0600

    first update

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from a container!",
+    service: "hello-node",
+    pod: process.env.POD_NAME || "unknown",
+    time: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
